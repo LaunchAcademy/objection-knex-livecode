3b9941699c65bf0eefa2149978d217ca84a421d1
Stephen Fang <safang99@gmail.com>
2021-01-12 14:41:28 -0500

    removed jsonSchema error handling [ch3250]

--- server/src/models/ComicBook.js
+++ server/src/models/ComicBook.js
@@ -4,20 +4,6 @@ class Birthday extends Model {
   static get tableName() {
     return "comic_books"
   }
-
-  static get jsonSchema() {
-    return {
-      type: "object",
-      required: ["title", "publisher", "genre", "firstEdition"],
-      properties: {
-        title: { type: "string" },
-        publisher: { type: "string" },
-        genre: { type: "string" },
-        firstEdition: { type: ["boolean", "string"] },
-        details: { type: "string" }
-      }
-    }
-  }
 }
 
 module.exports = Birthday

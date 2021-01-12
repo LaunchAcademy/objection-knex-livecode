04b11a602ef5f31cd2ea54a0f9de5dc0b4c63a20
Stephen Fang <safang99@gmail.com>
2021-01-12 14:44:18 -0500

    fix ComicBook class name

--- server/src/models/ComicBook.js
+++ server/src/models/ComicBook.js
@@ -1,23 +1,9 @@
 const Model = require("./Model")
 
-class Birthday extends Model {
+class ComicBook extends Model {
   static get tableName() {
     return "comic_books"
   }
-
-  static get jsonSchema() {
-    return {
-      type: "object",
-      required: ["title", "publisher", "genre", "firstEdition"],
-      properties: {
-        title: { type: "string" },
-        publisher: { type: "string" },
-        genre: { type: "string" },
-        firstEdition: { type: ["boolean", "string"] },
-        details: { type: "string" }
-      }
-    }
-  }
 }
 
-module.exports = Birthday
+module.exports = ComicBook
--- server/src/routes/api/v1/comicBooksRouter.js
+++ server/src/routes/api/v1/comicBooksRouter.js
@@ -1,9 +1,7 @@
 import express from "express"
 import objection from "objection"
-const { ValidationError } = objection
 
 import ComicBook from "../../../models/ComicBook.js"
-import cleanUserInput from "../../../services/cleanUserInput.js"
 
 const comicBooksRouter = new express.Router()
 
@@ -18,18 +16,11 @@ comicBooksRouter.get("/", async (req, res) => {
 
 comicBooksRouter.post("/", async (req, res) => {
   const { body } = req
-  console.log(body)
-  const formInput = cleanUserInput(body)
-  console.log("formInput")
-  console.log(formInput)
 
   try {
-    const newComicBook = await ComicBook.query().insertAndFetch(formInput)
+    const newComicBook = await ComicBook.query().insertAndFetch(body)
     return res.status(201).json({ newComicBook })
   } catch (error) {
-    if (error instanceof ValidationError) {
-      return res.status(422).json({ errors: error.data })
-    }
     return res.status(500).json({ errors: error })
   }
 })
